606d7f0406e3abf074048f4b7fb03d3b00499f92
Ramon Cortes <ramoncortes.varios@gmail.com>
2023-04-27 16:08:47 -0600

    express react 1

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <div>
+      Bad Bank in Heroku
+    </div>
+  );
+}
+
+export default App;
--- server.js
+++ server.js
@@ -5,14 +5,24 @@ import cors from 'cors';
 const app = express();
 app.use(cors());
 const PORT = process.env.PORT || 3141;
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 import chalk from 'chalk';
 // Probar y usar? ↓
 //app.use(express.urlencoded({ extended: true }));
 
 //FIRST TEST
 app.get('/', (req, res) => {
-  console.log(chalk.redBright('someone hit root'));
-  res.send('Hello World');
+  //res.sendFile(__dirname + '/frontend/index.html');
+  res.sendFile(__dirname + '/frontend/build/index.html');
 });
 
-app.listen(PORT, () => console.log(chalk.inverse(`Listening on port ${PORT}`)));
+// Serve static files
+//app.use(express.static('frontend'));
+
+app.listen(PORT, () => {
+  console.log(chalk.inverse(`Listening on port ${PORT}`));
+  //console.log(__dirname + '/frontend/index.html');
+});
